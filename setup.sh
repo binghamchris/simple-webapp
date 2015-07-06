639a73faea0125a92563d2cc2ecbfdc7af8c9249
Chris Bingham <chris@pearit.co.uk>
2015-07-06 20:29:51 +0200

    Update All Packages
    Added yum update command.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Install prerequisites for puppet runs
-yum install puppet git -y
+# Install prerequisites for Puppet runs
+sudo yum update -y
+sudo yum install puppet3 git -y
+sudo puppet module install jfryman-nginx
+
+# Clone the GitHub repo
+git clone https://github.com/binghamchris/simple-webapp.git
+
+# Apply the Puppet manifest
+sudo puppet apply ~/simple-webapp/main.pp
