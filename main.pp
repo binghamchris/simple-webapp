f52198e4535ddaf0f5b6cc0695f93a47a5187615
Chris Bingham <chris@pearit.co.uk>
2015-07-06 19:24:04 +0200

    Cleanup and setup
    Cleaned up Puppet manifest. Added script for initial setup prior to
    first Puppet run.

--- main.pp
+++ main.pp
@@ -1,61 +1,57 @@
 include nginx
 
-$www_root ='/var/www/default'
+$www_root = '/var/www/default'
+$index_file = 'index.php'
 
 package {'php':
-  ensure        => present,
+  ensure        => latest,
   allow_virtual => false,
 }
 
 package {'php-fpm':
-  ensure        => present,
+  ensure        => latest,
   allow_virtual => false,
+  require       => Package['php'],
 }
 
 service {'php-fpm':
-  ensure    => running,
-  enable    => true,
-  require   => Package['php-fpm'],
+  ensure  => running,
+  enable  => true,
+  require => Package['php-fpm'],
 }
 
 nginx::resource::vhost { 'default':
   ensure      => present,
   listen_port => 80,
-  index_files => ['index.php'],
+  index_files => ["${index_file}"],
   www_root    => $www_root,
   server_name => ['_'],
-  require     => File ['www_root', 'index.php'],
+  require     => File['www_root', 'index_file'],
 }
 
 nginx::resource::location { 'default_root':
-  ensure              => present,
-  vhost               => 'default',
-  location            => '~ \.php$',
-  www_root            => $www_root,
-  fastcgi             => '127.0.0.1:9000',
-  index_files         => ['index.php'],
-  fastcgi_script      => undef,
-  location_cfg_append => {
-    fastcgi_connect_timeout => '3m',
-    fastcgi_read_timeout    => '3m',
-    fastcgi_send_timeout    => '3m'
-  }
+  ensure      => present,
+  vhost       => 'default',
+  location    => '~ \.php$',
+  www_root    => $www_root,
+  fastcgi     => '127.0.0.1:9000',
+  index_files => ["${index_file}"],
 }
 
 file {'www_root':
-  path    => $www_root,
-  ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
+  path   => $www_root,
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
 }
 
-file {'index.php':
-  path    => "${www_root}/index.php",
+file {'index_file':
+  path    => "${www_root}/${index_file}",
   ensure  => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0755',
   content => '<?php echo "Hello AWS!"; ?>',
-  require => File ['www_root'],
+  require => File['www_root'],
 }
